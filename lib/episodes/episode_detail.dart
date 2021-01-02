15809fd90fa1ba0491bd7688dfe43e473d8110e8
stonega <xijieyin@gmail.com>
2021-01-02 22:50:44 +0800

    Remove unsed import.

--- lib/episodes/episode_detail.dart
+++ lib/episodes/episode_detail.dart
@@ -16,9 +16,9 @@ import '../state/audio_state.dart';
 import '../state/setting_state.dart';
 import '../type/episodebrief.dart';
 import '../type/play_histroy.dart';
+import '../util/extension_helper.dart';
 import '../widgets/audiopanel.dart';
 import '../widgets/custom_widget.dart';
-import '../util/extension_helper.dart';
 import 'episode_download.dart';
 
 class EpisodeDetail extends StatefulWidget {
