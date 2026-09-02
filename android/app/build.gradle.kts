a8e91ee748833b38fa090490345286379a038c72
stone <xijieyin@gmail.com>
2026-09-02 16:50:20 +0800

    build: add F-Droid ABI version codes

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,7 +1,16 @@
+import com.android.build.api.variant.FilterConfiguration.FilterType.ABI
+
 plugins {
     id("com.android.application")
     id("dev.flutter.flutter-gradle-plugin")
 }
+
+val abiVersionCodes =
+    mapOf(
+        "armeabi-v7a" to 1,
+        "arm64-v8a" to 2,
+        "x86_64" to 3,
+    )
 
 android {
     namespace = "com.stonegate.tsacdop"
@@ -46,6 +55,19 @@ android {
         }
         create("fdroid") {
             dimension = "deploy"
+        }
+    }
+}
+
+androidComponents {
+    onVariants { variant ->
+        variant.outputs.forEach { output ->
+            val abi = output.filters.find { it.filterType == ABI }?.identifier
+            val abiVersionCode = abiVersionCodes[abi]
+
+            if (abiVersionCode != null) {
+                output.versionCode.set(output.versionCode.get() * 10 + abiVersionCode)
+            }
         }
     }
 }
